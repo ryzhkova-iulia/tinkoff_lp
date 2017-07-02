378a301309ee8e54f6c61e31cf9eb8ee405d04f3
julia <ryzhkova.juliia@gmail.com>
2017-07-03 00:15:07 +0300

    add  changes

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -8,4 +8,24 @@ $(function () {
     $(".js-category").select2({
         theme : "default flat"
     });
+
+    $(".flat .select2-selection--multiple").mCustomScrollbar({
+        scrollInertia: 0,
+        alwaysShowScrollbar: 0,
+        axis:"y",
+        autoExpandScrollbar : true,
+        contentTouchScroll: true,
+        documentTouchScroll: true
+
+    });
+
+
+    $(".form-fieled input").on("change", function () {
+        var $formField = $(this).parent(".form-fieled");
+        if (this.value && this.value.length > 0) {
+            $formField.addClass("form-fieled_has-content");
+        } else {
+            $formField.removeClass("form-fieled_has-content");
+        }
+    }).trigger("change");
 });
